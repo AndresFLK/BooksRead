daf55240b66b28238d8c343651d99b6f6184fa5c
AndresFLK <andrescrptycol@gmail.com>
2023-11-21 16:54:43 -0600

    Intitial Files

---
+++ index.js
@@ -0,0 +1,17 @@
+// 1. Import express,body-parser,axios
+import axios from "axios";
+import bodyParser from "body-parser";
+import express from "express";
+
+// 2. Create an express app and set the port number.
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
