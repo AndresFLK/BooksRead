c14936d0f54d4f625bf6f8051bf4b33c1a26bf16
AndresFLK <andrescrptycol@gmail.com>
2023-11-21 18:58:33 -0600

    Styles PT-1

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
     res.render("index.ejs");
 });
